1bcc0884331d6cfa0e4bb64d5e6946b02543dd83
danyaiteens <danyaiteens@gmail.com>
2020-03-25 00:27:37 +0200

    fix

--- module1/task-7.js
+++ module1/task-7.js
@@ -26,3 +26,37 @@
 //   default:
 //     alert(`В страну "${country}" достава не доступна :(`);
 // }
+
+// let country;
+// country = prompt("Введите страну!").toLowerCase();
+
+// let countryPay;
+// switch (country) {
+//   case "китай":
+//     countryPay = 180;
+//     alert();
+//     break;
+
+//   case "чили":
+//     countryPay = 250;
+//     alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
+//     break;
+
+//   case "австралия":
+//     countryPay = 170;
+//     alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
+//     break;
+
+//   case "индия":
+//     countryPay = 80;
+//     alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
+//     break;
+
+//   case "ямайка":
+//     countryPay = 120;
+//     alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
+//     break;
+
+//   default:
+//     alert(`Доставка в ${country} будет стоить ${countryPay} кредитов`);
+// }
