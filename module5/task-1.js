025d6151a63f94706604906b245e69909ad3823c
AleksandraHrevtsova <aleksandra.hrevtsova@gmail.com>
2020-05-02 18:46:43 +0300

    gg

--- module5/task-1.js
+++ module5/task-1.js
@@ -1,90 +1,82 @@
 "use strict";
 
-// // ======================= 1 ==========================
+// ======================= 1 ==========================
 
-// // Напиши функцию-конструктор Account, которая создает объект
-// // со свойствами login и email. В prototype функции-конструктора
-// // добавь метод getInfo(), который выводит в консоль
-// // значения полей login и email объекта который его вызвал.
+// Напиши функцию-конструктор Account, которая создает объект
+// со свойствами login и email. В prototype функции-конструктора
+// добавь метод getInfo(), который выводит в консоль
+// значения полей login и email объекта который его вызвал.
 
+// ========== MINE ================
+const Account = function (login, email) {
+  this.login = login;
+  this.email = email;
+};
+
+Account.prototype.getInfo = function () {
+  return console.log(`email: ${this.email}, login: ${this.login} `);
+};
 // // ========== MINE ================
-// const Account = function (login, email) {
-//   this.login = login;
-//   this.email = email;
-// };
 
-// Account.prototype.getInfo = function () {
-//   return console.log(`email: ${this.email}, login: ${this.login} `);
-// };
-// // ========== MINE ================
+console.log(Account.prototype.getInfo); // function
+const mango = new Account("Mangozedog", "mango@dog.woof");
+mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+const poly = new Account("Poly", "poly@mail.com");
+poly.getInfo(); // Login: Poly, Email: poly@mail.com
 
-// console.log(Account.prototype.getInfo); // function
+// ======================= 2 ==========================
+// Напиши класс User для создания пользователя со следующим свойствами:
 
-// const mango = new Account("Mangozedog", "mango@dog.woof");
+// name - строка
+// age - число
+// followers - число
+// Добавь метод getInfo(), который, выводит строку: User ${имя} is ${возраст} years old and has ${кол-во фоловеров} followers
+// ========== MINE ================
+class User {
+  constructor(name, age, followers) {
+    this.name = name;
+    this.age = age;
+    this.followers = followers;
+  }
+  getInfo() {
+    console.log(
+      `User ${this.name} is ${this.age} years old and has ${this.followers} followers`
+    );
+  }
+}
+// ========== MINE ================
+const mango1 = new User("Mango", 2, 20);
+mango1.getInfo(); // User Mango is 2 years old and has 20 followers
 
-// mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+const poly1 = new User("Poly", 3, 17);
+poly1.getInfo(); // User Poly is 3 years old and has 17 followers
 
-// const poly = new Account("Poly", "poly@mail.com");
+// ======================= 3 ==========================
+// Напиши класс Storage, который будет создавать объекты для управления складом товаров. При вызове будет получать один аргумент -
+// начальный массив товаров, и записывать его в свойство items.
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+  // Добавь методы класса:
 
-// poly.getInfo(); // Login: Poly, Email: poly@mail.com
-
-// // ======================= 2 ==========================
-// // Напиши класс User для создания пользователя со следующим свойствами:
-
-// // name - строка
-// // age - число
-// // followers - число
-// // Добавь метод getInfo(), который, выводит строку: User ${имя} is ${возраст} years old and has ${кол-во фоловеров} followers
-// // ========== MINE ================
-// class User {
-//   constructor(name, age, followers) {
-//     this.name = name;
-//     this.age = age;
-//     this.followers = followers;
-//   }
-//   getInfo() {
-//     console.log(
-//       `User ${this.name} is ${this.age} years old and has ${this.followers} followers`
-//     );
-//   }
-// }
-// // ========== MINE ================
-// const mango1 = new User("Mango", 2, 20);
-
-// mango1.getInfo(); // User Mango is 2 years old and has 20 followers
-
-// const poly1 = new User("Poly", 3, 17);
-
-// poly1.getInfo(); // User Poly is 3 years old and has 17 followers
-
-// // ======================= 3 ==========================
-// // Напиши класс Storage, который будет создавать объекты для управления складом товаров. При вызове будет получать один аргумент -
-// // начальный массив товаров, и записывать его в свойство items.
-// class Storage {
-//   constructor(items) {
-//     this.items = items;
-//   }
-//   // Добавь методы класса:
-
-//   // getItems() - возвращает массив текущих товаров
-//   // addItem(item) - получает новый товар и добавляет его к текущим
-//   // removeItem(item) - получет товар и, если он есть, удаляет его из текущих
-//   getItems() {
-//     return this.items;
-//   }
-//   addItem(item) {
-//     this.items.push(item);
-//     return this.items;
-//   }
-//   removeItem(item) {
-//     for (let i = 0; i < this.items.length; i += 1) {
-//       if (this.items[i] === item) {
-//         this.items.splice(i, 1);
-//       }
-//     }
-//     return this.items;
-//   }
-// }
+  // getItems() - возвращает массив текущих товаров
+  // addItem(item) - получает новый товар и добавляет его к текущим
+  // removeItem(item) - получет товар и, если он есть, удаляет его из текущих
+  getItems() {
+    return this.items;
+  }
+  addItem(item) {
+    return this.items.push(item);
+  }
+  removeItem(item) {
+    for (let i = 0; i < this.items.length; i += 1) {
+      if (this.items[i] === item) {
+        return this.items.splice(i, 1);
+      }
+    }
+  }
+}
 
 // const storage = new Storage([
 //   "Нанитоиды",
@@ -103,48 +95,48 @@
 // storage.removeItem("Пролонгер");
 // console.table("removeItem", storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
 
-// // ======================= 4 ==========================
-// // Напиши класс StringBuilder. На вход он получает один параметр - строку,
-// // которую записывает в свойство _value.
+// ======================= 4 ==========================
+// Напиши класс StringBuilder. На вход он получает один параметр - строку,
+// которую записывает в свойство _value.
 
-// // Добавь классу следующий функционал:
+// Добавь классу следующий функционал:
 
-// // Геттер value - возвращает текущее значение поля _value
-// // Метод append(str) - получает парметр str (строку) и добавляет ее в конец _value
-// // Метод prepend(str) - получает парметр str (строку) и добавляет ее в начало value
-// // Метод pad(str) - получает парметр str (строку) и добавляет ее в начало и в конец _value
-// class StringBuilder {
-//   constructor(str) {
-//     this._value = str;
-//   }
-//   get value() {
-//     return this._value;
-//   }
-//   append(str) {
-//     console.log(typeof str);
-//     this._value += str;
-//     return this._value;
-//   }
-//   prepend(str) {
-//     this._value = str + this._value;
-//     return this._value;
-//   }
-//   pad(str) {
-//     this._value = str + this._value + str;
-//     return this._value;
-//   }
-// }
-// const builder = new StringBuilder(".");
-// console.log(builder.value);
+// Геттер value - возвращает текущее значение поля _value
+// Метод append(str) - получает парметр str (строку) и добавляет ее в конец _value
+// Метод prepend(str) - получает парметр str (строку) и добавляет ее в начало value
+// Метод pad(str) - получает парметр str (строку) и добавляет ее в начало и в конец _value
+class StringBuilder {
+  constructor(str) {
+    this._value = str;
+  }
+  get value() {
+    return this._value;
+  }
+  append(str) {
+    console.log(typeof str);
+    this._value += str;
+    return this._value;
+  }
+  prepend(str) {
+    this._value = str + this._value;
+    return this._value;
+  }
+  pad(str) {
+    this._value = str + this._value + str;
+    return this._value;
+  }
+}
+const builder = new StringBuilder(".");
+console.log(builder.value);
 
-// builder.append("^");
-// console.log(builder.value); // '.^'
+builder.append("^");
+console.log(builder.value); // '.^'
 
-// builder.prepend("^");
-// console.log(builder.value); // '^.^'
+builder.prepend("^");
+console.log(builder.value); // '^.^'
 
-// builder.pad("=");
-// console.log(builder.value); // '=^.^='
+builder.pad("=");
+console.log(builder.value); // '=^.^='
 
 // ======================= 5 ==========================
 // Напиши класс Car с указанными свойствами и методами.
